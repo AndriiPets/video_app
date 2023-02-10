19fdb8a6f9a3698fc1273971cbf5470be8f53a25
AndriiPets <nnggtrgftgd@gmail.com>
2023-02-10 23:25:05 +0200

    connected to api

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import styled, { ThemeProvider } from 'styled-components';
-import Menu from './components/Menu';
-import Navbar from './components/Navbar';
-import { darkTheme,lightTheme, Theme } from './utils/Theme'
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import styled, { ThemeProvider } from "styled-components";
+import Menu from "./components/Menu";
+import Navbar from "./components/Navbar";
+import { darkTheme, lightTheme, Theme } from "./utils/Theme";
 import {
   BrowserRouter,
   createBrowserRouter,
@@ -12,26 +12,25 @@ import {
   RouterProvider,
   Routes,
 } from "react-router-dom";
-import Home from './pages/Home';
-import Video from './pages/Video';
-import Signin from './pages/Signin';
+import Home from "./pages/Home";
+import Video from "./pages/Video";
+import Signin from "./pages/Signin";
 
 const Container = styled.div`
   display: flex;
-`
+`;
 
 const Main = styled.div`
-  flex:7;
-  background-color: ${({theme}) => theme.bgMain};
-`
+  flex: 7;
+  background-color: ${({ theme }) => theme.bgMain};
+`;
 
 const Wrapper = styled.div`
   padding: 22px 56px;
-`
+`;
 
 function App() {
-
-  const [ dark, setDark ] = useState(true)
+  const [dark, setDark] = useState(true);
 
   return (
     <ThemeProvider theme={dark ? darkTheme : lightTheme}>
@@ -42,11 +41,13 @@ function App() {
             <Navbar />
             <Wrapper>
               <Routes>
-                <Route path='/'>
-                  <Route index element={<Home />} />
-                  <Route path='signin' element={<Signin />} />
-                  <Route path='video'>
-                    <Route path=':id' element={<Video />} />
+                <Route path="/">
+                  <Route index element={<Home type="random" />} />
+                  <Route path="trends" element={<Home type="trend" />} />
+                  <Route path="subscriptions" element={<Home type="sub" />} />
+                  <Route path="signin" element={<Signin />} />
+                  <Route path="video">
+                    <Route path=":id" element={<Video />} />
                   </Route>
                 </Route>
               </Routes>
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,78 +1,73 @@
-import React, { useEffect } from 'react'
-import { Link } from 'react-router-dom';
-import styled from 'styled-components'
-
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { CardProps } from "../utils/Types";
 
 const Container = styled.div<CardProps>`
-    width: ${(prop) => prop.type !== 'sm' && '290px'};
-    margin-bottom: ${(prop) => (prop.type === 'sm' ? '10px' : '45px')};
-    cursor: pointer;
-    display: ${(prop) => prop.type === 'sm' && 'flex'};
-    gap: 10px;
-`
+  width: ${(prop) => prop.type !== "sm" && "290px"};
+  margin-bottom: ${(prop) => (prop.type === "sm" ? "10px" : "45px")};
+  cursor: pointer;
+  display: ${(prop) => prop.type === "sm" && "flex"};
+  gap: 10px;
+`;
 
 const Image = styled.img<CardProps>`
-    width: 100%;
-    height: ${(prop) => (prop.type === 'sm' ? '110px' : '192px')};
-    background-color: #999;
-    flex: 1;
-    border-radius: ${(prop) => prop.type === 'sm' ? '10px' : '0%'};
-`
+  width: 100%;
+  height: ${(prop) => (prop.type === "sm" ? "110px" : "192px")};
+  background-color: #999;
+  flex: 1;
+  border-radius: ${(prop) => (prop.type === "sm" ? "10px" : "0%")};
+`;
 const Details = styled.div<CardProps>`
-    display: flex;
-    margin-top: ${(prop) => prop.type !== 'sm' && '16px'};
-    gap: 12px;
-    flex: 1;
-`
+  display: flex;
+  margin-top: ${(prop) => prop.type !== "sm" && "16px"};
+  gap: 12px;
+  flex: 1;
+`;
 
 const ChannelImage = styled.img<CardProps>`
-    width: 36px;
-    height: 36px;
-    border-radius: 50%;
-    background-color: #999;
-    display: ${(prop) => prop.type === 'sm' && 'none'};
-`
-const Texts = styled.div`
-    
-`
+  width: 36px;
+  height: 36px;
+  border-radius: 50%;
+  background-color: #999;
+  display: ${(prop) => prop.type === "sm" && "none"};
+`;
+const Texts = styled.div``;
 const Title = styled.h1`
-    font-size: 16px;
-    font-weight: 500;
-    color: ${({theme}) => theme.text};
-`
+  font-size: 16px;
+  font-weight: 500;
+  color: ${({ theme }) => theme.text};
+`;
 const ChannelName = styled.h2`
-    font-size: 14px;
-    color: ${({theme}) => theme.textSoft};
-    margin: 9px 0px;
-`
+  font-size: 14px;
+  color: ${({ theme }) => theme.textSoft};
+  margin: 9px 0px;
+`;
 const Info = styled.div`
-    font-size: 14px;
-    color: ${({theme}) => theme.textSoft};
-`
-interface CardProps {
-  type: string
-}
+  font-size: 14px;
+  color: ${({ theme }) => theme.textSoft};
+`;
 
-function Card({type}: CardProps) {
+function Card({ type, video }: CardProps) {
   return (
-    <Link to='video/test' style={{textDecoration: "none"}}>
-    <Container type={type}>
-      <Image 
-      type={type}
-      src="https://i9.ytimg.com/vi_webp/k3Vfj-e1Ma4/mqdefault.webp?v=6277c159&sqp=CIjm8JUG&rs=AOn4CLDeKmf_vlMC1q9RBEZu-XQApzm6sA"/>
-      <Details type={type}>
-        <ChannelImage 
-        type={type}
-        src="https://yt3.ggpht.com/yti/APfAmoE-Q0ZLJ4vk3vqmV4Kwp0sbrjxLyB8Q4ZgNsiRH=s88-c-k-c0x00ffffff-no-rj-mo" />
-        <Texts>
-            <Title>Test video</Title>
+    <Link to="video/test" style={{ textDecoration: "none" }}>
+      <Container type={type} video={video}>
+        <Image type={type} video={video} src={video.imgUrl} />
+        <Details type={type} video={video}>
+          <ChannelImage
+            type={type}
+            video={video}
+            src="https://yt3.ggpht.com/yti/APfAmoE-Q0ZLJ4vk3vqmV4Kwp0sbrjxLyB8Q4ZgNsiRH=s88-c-k-c0x00ffffff-no-rj-mo"
+          />
+          <Texts>
+            <Title>{video.title}</Title>
             <ChannelName>Dev Dev</ChannelName>
-            <Info>660,908 views  1 day ago</Info>
-        </Texts>
-      </Details>
-    </Container>
+            <Info>{video.views} views 1 day ago</Info>
+          </Texts>
+        </Details>
+      </Container>
     </Link>
-  )
+  );
 }
 
-export default Card
+export default Card;
