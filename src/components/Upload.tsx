c3bd7c3c3bb1d4c84751fa69267967dbfc2ee10d
AndriiPets <nnggtrgftgd@gmail.com>
2023-02-21 23:45:34 +0200

    added upload element

---
+++ src/components/Upload.tsx
@@ -0,0 +1,88 @@
+import React, { Dispatch, SetStateAction } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #000000ac;
+`;
+
+const Wrapper = styled.div`
+  width: 600px;
+  height: 600px;
+  background-color: ${({ theme }) => theme.bgSide};
+  color: ${({ theme }) => theme.text};
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  position: relative;
+`;
+
+const Close = styled.div`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  cursor: pointer;
+`;
+
+const Title = styled.h1`
+  text-align: center;
+`;
+
+const Input = styled.input`
+  border: 1px solid ${({ theme }) => theme.soft};
+  color: ${({ theme }) => theme.text};
+  border-radius: 3px;
+  padding: 10px;
+  background-color: transparent;
+`;
+
+const Desc = styled.textarea`
+  border: 1px solid ${({ theme }) => theme.soft};
+  color: ${({ theme }) => theme.text};
+  border-radius: 3px;
+  padding: 10px;
+  background-color: transparent;
+`;
+
+const Button = styled.button`
+  border-radius: 3px;
+  border: none;
+  padding: 10px 20px;
+  font-weight: 500;
+  cursor: pointer;
+  background-color: ${({ theme }) => theme.soft};
+  color: ${({ theme }) => theme.text};
+`;
+
+const Lable = styled.label`
+  font-size: 14px;
+`;
+
+function Upload({ setOpen }: { setOpen: Dispatch<SetStateAction<boolean>> }) {
+  return (
+    <Container>
+      <Wrapper>
+        <Close onClick={() => setOpen(false)}>X</Close>
+        <Title>Upload a New Video</Title>
+        <Lable>Video:</Lable>
+        <Input type="file" accept="video/*" />
+        <Input type="text" placeholder="Title" />
+        <Desc placeholder="Description" rows={8} />
+        <Input type="text" placeholder="Separate tags with commas." />
+        <Lable>Thumbnail:</Lable>
+        <Input type="file" accept="image/*" />
+        <Button>Upload</Button>
+      </Wrapper>
+    </Container>
+  );
+}
+
+export default Upload;
